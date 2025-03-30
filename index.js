66935dba2e7d387775593a4b6d59ebb6c77ef730
fahmazp <fahmazashraf7@gmail.com>
2025-03-30 21:51:12 +0530

    cors origin updated in index.js

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",credentials:true,methods:["GET","POST","PUT","DELETE","OPTION"]
+    origin:["http://localhost:5173", "https://movie-review-frontend-ten.vercel.app"],
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE","OPTION"]
 }))
 
 app.get('/', (req, res) => {
